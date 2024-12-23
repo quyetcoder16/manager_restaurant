e4c7f07e0502066c31db52a4e20aebc31ee356b0
dxquyet16 <quyetcoder16@gmail.com>
2024-12-23 21:48:08 +0700

    first commit

---
+++ src/main/java/com/promise/manager_restaurant/ManagerRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.promise.manager_restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagerRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagerRestaurantApplication.class, args);
+	}
+
+}
